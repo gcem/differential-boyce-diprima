57daea4559dafd3836762be5567b5c33fad12f0e
gcem <34423323+gcem@users.noreply.github.com>
2021-07-01 23:41:46 +0300

    My own quiver implementation (without arrows) for direction fields

--- dirfield.m
+++ dirfield.m
@@ -11,7 +11,24 @@ function dirfield(f, dif, ymin, ymax)
   vx = ones(size(xx));
   vy = f(xx, yy);
   norm = sqrt(vx .* vx + vy .* vy);
-  quiver(xx, yy, vx ./ norm, vy ./ norm, 0.8);
+  vx = vx ./ norm * (dif / 2 * 1);
+  vy = vy ./ norm * (dif / 2 * 1);
+##  quiver(xx, yy, vx ./ norm, vy ./ norm, 0.8);
+  
+  startx = xx - vx;
+  endx = xx + vx;
+  starty = yy - vy;
+  endy = yy + vy;
+  
+  n = numel(startx);
+  # this way (drawing separate lines) is very slow
+  # so I put NaNs in between and draw a 'single' row
+##  x = [startx(:)'; endx(:)'];
+##  y = [starty(:)'; endy(:)'];
+  x = [startx(:)'; endx(:)'; NaN(1,n)](:);
+  y = [starty(:)'; endy(:)'; NaN(1,n)](:);
+  plot(x, y, 'b')
+  
   grid minor on;
   grid on;
   xlim([xmin, xmax])
--- ob.m
+++ ob.m
@@ -1,9 +1,9 @@
-function ob(slope = 1)
+function ob(slope = 1, color = '')
   x = xlim;
   y = x * slope;
   h = ishold;
   hold on
-  plot(x, y);
+  plot(x, y, color);
   if !h
     hold off
   endif
