9af71d67bd0314872db72fa8480eacac81739e80
gcem <34423323+gcem@users.noreply.github.com>
2021-07-01 23:39:50 +0300

    Some extra (?) files for section 2.4

---
+++ sec2-4/q18.m
@@ -0,0 +1,33 @@
+## Copyright (C) 2021 cem
+## 
+## This program is free software: you can redistribute it and/or modify it
+## under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+## 
+## This program is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+## 
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see
+## <https://www.gnu.org/licenses/>.
+
+## -*- texinfo -*- 
+## @deftypefn {} {@var{retval} =} q18 (@var{input1}, @var{input2})
+##
+## @seealso{}
+## @end deftypefn
+
+## Author: cem <cem@debian>
+## Created: 2021-06-23
+
+function retval = q18 (input1, input2)
+  f = @(t,y) y .* (3 - t .* y);
+  df(f,0.2)
+  ax
+  hold on
+  plot([1,3], [3, 1], 'or'); # pts on y = 3/t
+  hold off
+endfunction
---
+++ sec2-4/q20.m
@@ -0,0 +1,30 @@
+## Copyright (C) 2021 cem
+## 
+## This program is free software: you can redistribute it and/or modify it
+## under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+## 
+## This program is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+## 
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see
+## <https://www.gnu.org/licenses/>.
+
+## -*- texinfo -*- 
+## @deftypefn {} {@var{retval} =} q20 (@var{input1}, @var{input2})
+##
+## @seealso{}
+## @end deftypefn
+
+## Author: cem <cem@debian>
+## Created: 2021-06-25
+
+function retval = q20 (input1, input2)
+  f = @(t, y) t - 1 - y .* y;
+  df(f, .1, -5, 5)
+  ax
+endfunction
