86fec922b209118113c3e5a0bafab8f2f681364e
gcem <34423323+gcem@users.noreply.github.com>
2021-07-09 21:05:56 +0300

    Section 2.6 (one question only)

---
+++ sec2-6/q14.m
@@ -0,0 +1,33 @@
+## Copyright (C) 2021 cem
+## 
+## This program is free software: you can redistribute it and/or modify it
+## under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+## 
+## This program is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+## 
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see
+## <https://www.gnu.org/licenses/>.
+
+## -*- texinfo -*- 
+## @deftypefn {} {@var{retval} =} q14 (@var{input1}, @var{input2})
+##
+## @seealso{}
+## @end deftypefn
+
+## Author: cem <cem@debian>
+## Created: 2021-07-07
+
+function retval = q14 (input1, input2)
+  x = linspace(.8, 1.2, 1000);
+  d = 24*x.*x.*x + x.*x - 8*x -16;
+  plot(x,d)
+  xlim([.8, 1.2])
+  ax
+  grid minor
+endfunction
