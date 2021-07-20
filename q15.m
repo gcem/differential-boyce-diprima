3d3f96b716653fce5cae593e2d9a8514f8bff968
gcem <34423323+gcem@users.noreply.github.com>
2021-07-20 15:45:40 +0300

    S3.4 Q15 with wrong c value

---
+++ q15.m
@@ -0,0 +1,16 @@
+function q15
+  y = @(t) exp(-3/2*t) .* (1 - 5 * t);
+  t = linspace(0, 5, 10000);
+  yt = y(t);
+  plot(t, yt)
+  grid minor
+  ax
+  
+  t0 = fsolve(y, 0);
+  ymin = min(yt);
+  tmin = t(ymin == yt);
+  vert(t0)
+  vert(tmin)
+  hor(ymin)
+  legend show
+endfunction
