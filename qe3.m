80f00fbc2c5c4d022edd766d4e9f08623719a7ce
gcem <34423323+gcem@users.noreply.github.com>
2021-07-28 16:47:15 +0300

    Example 3

---
+++ qe3.m
@@ -0,0 +1,15 @@
+function qe3
+  t = linspace(0, 30*pi, 1000);
+  a = .5 / .36;
+  c1 = -a;
+  u = @(t) c1 * cos(t) + a * cos(0.8 * t);
+  
+  clf
+  hold on
+  plot(t, u(t));
+  plot(t, 2 * .5 / (1 - .64) * sin(.1 * t))
+  plot(t, -2 * .5 / (1 - .64) * sin(.1 * t))
+  ax 
+  grid on
+  
+endfunction
