db49a3c608a8925520980cd3ee159f8cc998c9a4
gcem <34423323+gcem@users.noreply.github.com>
2021-08-16 17:21:06 +0300

    A trivial change

--- q11.m
+++ q11.m
@@ -3,7 +3,7 @@ function q11
 ##  syms t s;
 ##  y(t) = exp(t) / 2 * int(exp(-s)/cos(s), s, 0, t) - t / 2 * (sin(t) + cos(t)) + (sin(t) - cos(t)) / 2 * log(cos(t)) + 3*exp(t) / 2 + cos(t) / 2 - 5 * sin(t) / 2
 
-  t = linspace(0, pi / 2 * .99999); # must start at 0, for lsode
+  t = linspace(0, pi / 2, 10000)(1:end-1); # must start at 0, for lsode
   integralValues = lsode(@(y, t) 1 ./ (exp(t) * cos(t)), 0, t)';
   y = exp(t) / 2 .* integralValues - t / 2 .* (sin(t) + cos(t)) + ...
     (sin(t) - cos(t)) / 2 .* log(cos(t)) + ...
