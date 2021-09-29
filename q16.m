49b850d2cbe0f40ce57d89a44cc0b6dbef1713e3
gcem <34423323+gcem@users.noreply.github.com>
2021-09-29 20:56:15 +0300

    Q16e

--- q16.m
+++ q16.m
@@ -1,6 +1,6 @@
 %%% rest of the solution is in wxmaxima
 function q16
-  t = linspace(0, 5*pi, 1000);
+  t = linspace(0, 10*pi, 10000);
   phi = @(t) 1 - exp(-t/8) .* (cos(sqrt(63/64)*t) + sqrt(64/63)/8 * sin(sqrt(63/64)*t));
   g = us(t-3/2) - us(t-5/2);
   
@@ -15,6 +15,15 @@ function q16
   vert(3.467484038650195) % from maxima
   hor(0.7992282796567876, 'style', '-.')
   hold off
+  
+  hor(.1, 'style', 'r--')
+  hor(-.1, 'style', 'r--')
+
+  y2 = @(t) 2*(us(t - 3/2) .* phi(t - 3/2) - us(t - 5/2) .* phi(t - 5/2));  
+  mint = fminbnd(y2, 25.6, 25.7)
+  miny = y2(mint)
+  tau = fsolve(@(t) 0.1 + y2(t), mint) % maxima says this is 25.67726
+  vert(tau)
   
   legend show
   pitics
