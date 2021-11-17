6513e6a8f4c4222f07e828db75b2f35801e3fb1a
gcem <34423323+gcem@users.noreply.github.com>
2021-11-17 15:33:47 +0300

    Q24

---
+++ q24.m
@@ -0,0 +1,1 @@
+function q24  f = @(t,c1,c2) [-1;2]*exp(-1*t) * c1 + [1;2] * exp(-2*t) * c2;  slns2(f)  xlim([-5,5])  ylim([-5,5])  ax  plot(2,3, 'or', 'linewidth', 2)  xx = f(linspace(-1,5,1000), -1/4, 7/4);  plot(xx(1,:), xx(2,:), 'k--')endfunction
