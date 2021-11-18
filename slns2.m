ea83b0fc4ed433a2c34ad22302a72feba48f7b40
gcem <34423323+gcem@users.noreply.github.com>
2021-11-18 16:06:53 +0300

    Disable legend in slns2 and increase point count

--- slns2.m
+++ slns2.m
@@ -3,7 +3,7 @@
 %% f should take a row vector and return a 2xn matrix
 function slns2(f = @(t, c1, c2) [t;t], c1s = linspace(-1,1,5), c2s = c1s, ...
    tmin = -5, tmax = 5)
-  t = linspace(tmin, tmax, 100);
+  t = linspace(tmin, tmax, 1000);
   solutionCount = length(c1s) * length(c2s);
   x1 = zeros(length(t), solutionCount);
   x2 = x1;
@@ -17,5 +17,6 @@ function slns2(f = @(t, c1, c2) [t;t], c1s = linspace(-1,1,5), c2s = c1s, ...
       j++;
     endfor
   endfor
+  legend off
   plot(x1, x2, 'linewidth', 2);
 endfunction
