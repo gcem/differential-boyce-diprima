1fb9e47d76d19e4abd7b559c609a0896fe4059eb
gcem <34423323+gcem@users.noreply.github.com>
2021-11-22 16:43:22 +0300

    Fix Q7

--- q7.m
+++ q7.m
@@ -1,11 +1,14 @@
 function q7
   A = [1 0 0; 2 1 -2; 3 2 1];
-  df3(A, 1, 10)
+  df3(A, 3, 10)
   slns3(@f);
+  xlabel x_1
+  ylabel x_2
+  zlabel x_3
 endfunction
 
 function x = f(t, c1, c2, c3)
   c = cos(2*t);
   s = sin(2*t);
-  x = exp(t) .* (c1*[2;-3;2] + c2*[0*t;c;-s] + c3*[0*t;s;c]);
+  x = exp(t) .* (c1*[2;-3;2] + c2*[0*t;c;s] + c3*[0*t;s;-c]);
 endfunction
