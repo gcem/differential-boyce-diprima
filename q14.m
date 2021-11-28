502e1df3d4b13faed4b3dc86e6360ef403c5a780
gcem <34423323+gcem@users.noreply.github.com>
2021-11-28 18:18:25 +0300

    Handle real roots correctly in Q14

--- q14.m
+++ q14.m
@@ -22,8 +22,18 @@ function f(alpha)
   ylabel x_2
   
   [v, r] = eig(A, 'vector');
-  r1 = r(1);
-  v1 = v(:,1);
-  sln = @(t, c1, c2) c1 * real(exp(t*r1) .* v1) + c2 * imag(exp(t*r1) .* v1);
+  sln = 0; # declare it
+  if imag(r(1)) != 0
+    r1 = r(1);
+    v1 = v(:,1);
+    sln = @(t, c1, c2) c1 * real(exp(t*r1) .* v1) + c2 * imag(exp(t*r1) .* v1);
+  else
+    r1 = r(1);
+    r2 = r(2);
+    v1 = v(:,1);
+    v2 = v(:,2);
+    sln = @(t, c1, c2) c1 * exp(t*r1) .* v1 + c2 * exp(t*r2) .* v2;
+  endif
+  
   slns2(sln, -6:3:6, -6:3:6, -40, 40)
 endfunction
