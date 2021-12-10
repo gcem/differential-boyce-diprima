72590d6f63bb0fbbdc368750885a677e530bd875
gcem <34423323+gcem@users.noreply.github.com>
2021-12-10 14:52:55 +0300

    Ex2 phase portrait

--- qe2.m
+++ qe2.m
@@ -2,4 +2,13 @@ function qe2
   A = [1 -1; 1 3];
   df2(A)
   ax
+
+  slns2(@sln);
+endfunction
+
+function x = sln(t,c1,c2)
+  ex = exp(2*t);
+  x = ...
+    c1 * ([1;-1] * (t.*ex) + [0;-1] * ex) + ...
+    c2 * [1;-1] * ex;
 endfunction
