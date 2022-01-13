270f83b5e06449f01d845b7e28678189916d0062
gcem <34423323+gcem@users.noreply.github.com>
2022-01-13 19:30:21 +0300

    Make hor and vert return handles when requested

--- hor.m
+++ hor.m
@@ -1,19 +1,22 @@
-function hor(varargin)
+function handleRet = hor(varargin)
   p = inputParser();
   p.addRequired('y', @isnumeric)
   p.addParameter('name', '__auto', @ischar);
   p.addParameter('style', '', @ischar);
   p.parse(varargin{:});
-  
+
   name = p.Results.name;
   if strcmp(name, '__auto')
     name = sprintf('y = %f', p.Results.y);
   endif
-  
+
   h = ishold;
   hold on;
-  plot(xlim, [1 1] * p.Results.y, p.Results.style, 'displayname', name);
+  handle = plot(xlim, [1 1] * p.Results.y, p.Results.style, 'displayname', name);
   if !h
     hold off;
+  endif
+  if nargout > 0
+    handleRet = handle;
   endif
 endfunction
--- vert.m
+++ vert.m
@@ -1,19 +1,22 @@
-function vert(varargin)
+function handleRet = vert(varargin)
   p = inputParser();
   p.addRequired('x', @isnumeric)
   p.addParameter('name', '__auto', @ischar);
   p.addParameter('style', '', @ischar);
   p.parse(varargin{:});
-  
+
   name = p.Results.name;
   if strcmp(name, '__auto')
     name = sprintf('x = %f', p.Results.x);
   endif
-  
+
   h = ishold;
   hold on;
-  plot([1 1] * p.Results.x, ylim, p.Results.style, 'displayname', name);
+  handle = plot([1 1] * p.Results.x, ylim, p.Results.style, 'displayname', name);
   if !h
     hold off;
+  endif
+  if nargout > 0
+    handleRet = handle;
   endif
 endfunction
