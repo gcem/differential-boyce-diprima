bf58026b32fa6412687d1b55a253f8304584b794
gcem <34423323+gcem@users.noreply.github.com>
2022-01-18 20:25:16 +0300

    Q1 completed (Backward Euler)

--- q1.m
+++ q1.m
@@ -16,6 +16,8 @@ function q1
   for hi = 1:length(hValues)
     h = hValues(hi);
     t = 0:h:max(tValues);
+
+    # euler
     y = zeros(size(t));
     y(1) = 1;
     for i = 1:(length(t) - 1)
@@ -25,16 +27,28 @@ function q1
     plot(t, y, 'displayname',
       sprintf('h = %.3f', h));
     yValues{hi} = y;
+
+    # backward euler
+    ybw = zeros(size(t));
+    ybw(1) = 1;
+    for i = 1:(length(t) - 1)
+      ybw(i + 1) = (ybw(i) + h * (3 + t(i) + h)) / (1 + h);
+    endfor
+    subplot(1,2,2)
+    plot(t, ybw, 'displayname',
+      sprintf('h = %.3f', h));
+    yValuesBw{hi} = ybw;
   endfor
 
   # print header row
-  printf('Euler''s Method\n');
   printf('|   h   |');
   printf(' %7.2f |', tValues);
   printf('\n');
   printf(char('-' * ones(1, 7 + 10 * length(tValues))));
   printf('\n');
 
+  # print euler's method
+  printf('(Euler Method)\n');
   for hi = 1:length(hValues)
     h = hValues(hi);
     printf('| %.3f |', h);
@@ -46,7 +60,23 @@ function q1
     printf('\n');
   endfor
 
+  # print backward euler's method
+  printf('(Backward Euler Method)\n');
+  for hi = 1:length(hValues)
+    h = hValues(hi);
+    printf('| %.3f |', h);
+    for t = tValues
+      ind = round(t / h) + 1;
+      val = yValuesBw{hi}(ind);
+      printf(' %6.5f |', val);
+    endfor
+    printf('\n');
+  endfor
 
+  subplot(1,2,1)
+  legend show
+  grid on
+  subplot(1,2,2)
   legend show
   grid on
 endfunction
